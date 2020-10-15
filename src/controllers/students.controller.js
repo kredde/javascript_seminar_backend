1f95021bd39ac127d695e9351fb53a0efedd50df
zhouh <ge49wej@tum.de>
2020-10-15 23:55:55 +0200

    do some to studentsController

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -2,18 +2,18 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { createStudentNotification } = require('../utils/notifications');
-const { userService, tokenService, notificationService } = require('../services');
+const { userService, tokenService, notificationService, studentService } = require('../services');
 
 const createStudent = catchAsync(async (req, res) => {
-  const student = await userService.createUser({ ...req.body, role: 'student' })
-  const token = await tokenService.generateAuthTokens(student);
-  const receiverNotification = createStudentNotification(token);
-  await notificationService.sendNotification(messageBody.to, receiverNotification);
-  res.send(student);
+  const student = await userService.createUser({ ...req.body, role: 'student' });
+  const tokens = await tokenService.generateAuthTokens(student);
+  const receiverNotification = createStudentNotification(tokens);
+  await notificationService.sendNotification(student._id, receiverNotification);
+  res.status(httpStatus.CREATED).send({ student, tokens });
 });
 
 const getStudent = catchAsync(async (req, res) => {
-  const student = await userService.getUserById(req.params.studentId);
+  const student = await studentService.getStudent(req.user._id, req.params.studentId);
   if (!student) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Student not found');
   }
@@ -21,8 +21,8 @@ const getStudent = catchAsync(async (req, res) => {
 });
 
 const updateStudent = catchAsync(async (req, res) => {
-  const _student = await userService.addStudentInformation(req.params.studentId, req.body);
-  res.send(_student);
+  const student = await studentService.updateStudent(req.user._id, req.params.studentId, req.body);
+  res.send(student);
 });
 
 module.exports = {
