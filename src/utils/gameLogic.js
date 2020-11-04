878055e5f234c2a5fdc2d41ae879992a6d37b937
Michaela Finger <m.finger@tum.de>
2020-11-04 16:35:22 +0100

    disable log when student disconnects from game

--- src/utils/gameLogic.js
+++ src/utils/gameLogic.js
@@ -24,7 +24,7 @@ module.exports = {
       });
 
       socket.on('disconnect', (data) => {
-        console.log('Disconnecting', data, socket);
+        //console.log('Disconnecting', data, socket);
         connectedUsers.delete(socket.userId);
       });
 
